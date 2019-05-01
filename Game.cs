59869b679cb9ea29d015c977e6cc5b7c19bb1a2a
Tate Reynolds <tate.reynolds@gmail.com>
2019-05-01 11:13:37 -0600

    Added two classes

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace KnightsOfNalleywey
+{
+    public class Game
+    {
+        //Character's name
+        private string characterName;
+
+        public Game()
+        {
+        }
+
+        //Set's the character's name
+        public void SetCharacterName(string _characterName)
+        {
+            this.characterName = _characterName;
+        }
+
+        //Game start text sequence
+        public void StartGame()
+        {
+            Console.WriteLine("The Knights Of Nallywey");
+            Console.WriteLine("\nDeep in the Middlowground Forest sits a four-eyed warlock \nthat has the power to turn lead into gold...\n");
+            Console.WriteLine("Enter your knight's name: ");
+            SetCharacterName(Console.ReadLine());
+            Console.WriteLine($"Good day Sir {characterName}! I'm Sage Kashmir. Pleased to meet you, friend!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace KnightsOfNalleywey
+{
+    public class Item
+    {
+        public Item()
+        {
+        }
+    }
+}
