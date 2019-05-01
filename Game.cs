f7f8a0098805cbb2deb98e9533d93141517bfc01
Tate Reynolds <tate.reynolds@gmail.com>
2019-05-01 11:57:21 -0600

    Color things

--- Game.cs
+++ Game.cs
@@ -5,6 +5,8 @@ namespace KnightsOfNalleywey
     {
         //Character's name
         private string characterName;
+        private const ConsoleColor CHAR_COLOR = ConsoleColor.DarkMagenta;
+
 
         public Game()
         {
@@ -15,27 +17,47 @@ namespace KnightsOfNalleywey
         public void SetCharacterName(string _characterName)
         {
             this.characterName = _characterName;
+           
         }
 
         //Asks for the characters name from the user
         private void AskName()
         {
-            Console.Write("Enter your knight's name: ");
+            Dialogue("What do you call yourself? ", ConsoleColor.DarkCyan);
+            Console.ForegroundColor = CHAR_COLOR;
             SetCharacterName(Console.ReadLine());
+            Console.ResetColor();
         }
         //Game start text sequence
         public void StartGame()
         {
+            Console.Title = "The Knights of Nalleywey";
             Console.WriteLine("The Knights Of Nallywey");
             Console.WriteLine("\nDeep in the Middlowground Forest sits a four-eyed warlock \nthat has the power to turn lead into gold...\n");
             this.AskName();
-            Console.WriteLine($"Good day Sir {this.characterName}! I'm Sage Kashmir. Pleased to meet you, friend!");
+            Dialogue($"Good day Sir ", ConsoleColor.DarkCyan);
+            UserDialogue(this.characterName);
+            Dialogue("! I'm Sage Kashmir. Pleased to meet you, friend!", ConsoleColor.DarkCyan);
             Console.ReadKey();
         }
 
+        //uncolored dialogue
+        public void Dialogue(string dialogue)
+        {
+            Console.Write(dialogue);
+        }
+
+        //colored dialogue
         public void Dialogue(string dialogue, ConsoleColor color)
         {
             Console.ForegroundColor = color;
+            Console.Write(dialogue);
+            Console.ResetColor();
+        }
+
+        public void UserDialogue(string dialogue)
+        {
+            Console.ForegroundColor = CHAR_COLOR;
             Console.Write(dialogue);
             Console.ResetColor();
         }
--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace KnightsOfNalleywey
         {
             Game newGame = new Game();
             newGame.StartGame();
+
            
         }
     }
