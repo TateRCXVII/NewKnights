d06d530feb5c9671c6ecc306597cba8b8d887efd
Tate Reynolds <tate.reynolds@gmail.com>
2019-04-30 22:03:04 -0600

    text

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace KnightsOfNalleywey
             Console.WriteLine("\nDeep in the Middlowground Forest sits a four-eyed warlock \nthat has the power to turn lead into gold...\n");
             Console.WriteLine("Enter your knight's name: ");
             CharacterName = Console.ReadLine();
-            Console.WriteLine($"Good day Sir {CharacterName}! I'm Sage Kashmir. Pleased to meet you!");
+            Console.WriteLine($"Good day Sir {CharacterName}! I'm Sage Kashmir. Pleased to meet you, friend!");
             Console.ReadKey();
         }
     }
